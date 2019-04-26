7dd974309c303a0e3506a4aaf80704c611f79900
hansenjl <jenn.leigh.hansen@gmail.com>
2019-04-26 11:08:42 -0700

    Study Group updates

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,38 @@
 class SessionsController < ApplicationController
-
+  #loggin in, logging out
 
   get '/supersecret' do
-    erb :'/sessions/secrets'
+    #check to see if someone is logged_in
+    if logged_in?
+      erb :'/sessions/secrets'
+    else
+      redirect '/'
+    end
+  end
+
+  get '/login' do
+    erb :'/sessions/login'
+  end
+
+  #handle logging in
+  post '/login' do
+
+    #find the user
+    user = User.find_by(username: params[:username])
+
+    # does the user exist & did they put in the right password?
+    if user && user.authenticate(params[:password])
+      #log the user in
+      session[:user_id] = user.id
+      redirect '/supersecret'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+  #signup
+
+  get '/signup' do
+    #load the signup form
+    erb :'/users/new'
+  end
+
+  #receive our signup form submission
+  post '/users' do
+    # make sure they filled out the form entirely
+    if params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+    end
+
+    # make sure they chose a username that is unique
+    if User.find_by(username: params[:username])
+      redirect '/signup'
+    end
+
+    # create a user
+    user = User.create(params)
+
+    # log the user in
+    session[:user_id] = user.id
+
+    # redirect them elsewhere
+    redirect '/supersecret'
+  end
+
+
 end
